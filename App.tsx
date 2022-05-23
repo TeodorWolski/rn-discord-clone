80a901ac42bbb4503f6199d6f64e3717e619d065
TeodorWolski <teodorwolski88@gmail.com>
2022-05-23 15:48:20 +0200

    Stream chat configured

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
+import { useEffect } from "react";
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/navigation";
+import { StreamChat } from "stream-chat";
+
+const API_KEY = "nxaurv9xrg56";
+
+const client = StreamChat.getInstance(API_KEY);
+
+const connectUser = async () => {
+  await client.connectUser(
+    {
+      id: "jlahey",
+      name: "Jim Lahey",
+      image: "https://i.imgur.com/fR9Jz14.png",
+    },
+    client.devToken("jlahey")
+  );
+
+  const channel = client.channel("team", "general", {
+    members: ["jlahey"],
+    name: "Awesome channel about traveling",
+  });
+
+  channel.create();
+};
 
 export default function App() {
+  useEffect(() => {
+    connectUser();
+  }, []);
+
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
